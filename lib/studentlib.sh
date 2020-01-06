62ad754adbf36904cdc1ac1aad0f41da249f5e98
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-06 15:10:47 -0600

    added extra studentlib functions

--- lib/studentlib.sh
+++ lib/studentlib.sh
@@ -16,11 +16,42 @@ last () {
     cat $COURSE_DB/students/$1/last
 }
 
+call_me () {
+    cat $COURSE_DB/students/$1/call-me
+}
+
 name () {
     echo `first $1; last $1`
 }
 
+called () {
+    call_me $1 || name $1
+}
+
 github () {
     cat $COURSE_DB/students/$1/github
+}
+
+find_student_by_name () {
+    eval "$(getargs + last first)"
+
+    find_single "student $first $last" \
+        $(filter_students_by_name "$last" "$first")
+}
+
+filter_students_by_name () {
+    eval "$(getargs + last first)"
+    local pattern
+
+    first=$(echo "$first" | sed 's/[^A-Za-z].*//')
+    last=$(echo "$last" | sed 's/[^A-Za-z].*//')
+    pattern=":$last.*,$first"
+
+    "$COURSE_BIN/all_students.sh" |
+        while read netid; do
+            echo "$netid:$(last $netid),$(first $netid)"
+        done |
+        grep -i -E "$pattern" |
+        sed 's/:.*//'
 }
 
