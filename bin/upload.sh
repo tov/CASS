bdada0a9cf8311f82f9db201d94fda3fb2e215d4
Jesse A. Tov <jesse.tov@gmail.com>
2020-01-06 15:00:33 -0600

    Removed duplication; ignoring nonexistent dir.

--- bin/upload.sh
+++ bin/upload.sh
@@ -16,50 +16,11 @@ assert_branch () {
     local actual
 
     eval "$(getargs dir desired)"
-    actual=$(cd "$dir" && git symbolic-ref HEAD) || exit 1
 
-    if [ "$actual" != "$desired" ]; then
-        exec >&2
-        echo "$(basename $0): cannot upload from this git branch"
-        if [ "$dir" != . ]; then
-            echo "  in submodule: $dir"
-        fi
-        echo "  branch is:    $actual"
-        echo "  should be:    $desired"
-        exit 2
-    fi
-}
-
-src=web
-dst=/tmp/upload
-man_host=batgirl.eecs.northwestern.edu
-
-assert_branch .       refs/heads/master
-assert_branch web/lab refs/heads/master
-
-if [ -n "$flag_m$flag_W" ]; then
-    if [ -z "$MAN_HOST" ]; then
-        MAN_HOST="$man_host"
-        echo >&2 "\$MAN_HOST not set; using $MAN_HOST"
+    if ! [ -d "$dir" ]; then
+        return
     fi
 
-    chmod -R a+rX man
-    tar cp man | ssh "$MAN_HOST" 'tar xpvC pub/share'
-fi
-
-if [ -z "$flag_W" ]; then
-    rm -Rf $dst
-    trap 'rm -Rf $dst' EXIT
-
-    "$COURSE_BIN"/publish.sh $src $dst &&
-        rsync -avz --delete --chmod=a+rX $dst/ $web_host:$web_path
-fi
-assert_branch () {
-    local dir
-    local desired
-    local actual
-
-    eval "$(getargs dir desired)"
     actual=$(cd "$dir" && git symbolic-ref HEAD) || exit 1
 
     if [ "$actual" != "$desired" ]; then
