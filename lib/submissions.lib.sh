c1cbbb6b3bd79501e141130baa59da7744d9c46f
Jesse A. Tov <jesse.tov@gmail.com>
2020-02-29 21:35:36 -0600

    Renamed some library functions; added some.

--- lib/submissions.lib.sh
+++ lib/submissions.lib.sh
@@ -2,24 +2,65 @@ course_use find
 
 info_file=grading-info.txt
 
+# $1: property heading
+# $2...: grading repo directories
+get_repo_prop () {
+    local prop; prop=$1; shift
+    local repo
+    for repo; do
+        sed "
+            /^$prop: */! d
+            s///
+            q
+        " "$repo/$info_file"
+    done
+}
+
+# $1: property heading
+# $2: new property value
+# $3...: grading repo directories
+set_repo_prop () {
+    local prop; prop=$1; shift
+    local valu; valu=$1; shift
+    local repo
+    for repo; do
+        sed -i.bak "s/^\\($prop\\):.*/\\1: $valu/" "$repo/$info_file"
+    done
+}
+
+# $1: grading repo directory
+repo_team_id () {
+    get_repo_prop 'Team ID' "$@"
+}
+
+# $1: grading repo directory
+repo_grader () {
+    get_repo_prop 'Grader' "$@"
+}
+
+# $1: hw
+# $2: netid
+netid_team () {
+    gsc admin partners hw$1 $2
+}
+
 # $*: netid...
 team_tag () {
     local divider=
     local partner
 
     for partner; do
-        printf %s%s "$divider" $(student_tag $partner)
+        printf %s%s "$divider" $(student_tag_ $partner)
         divider=_
     done
 }
 
-# $1: grading repo directory
-team_id () {
-    sed '
-        /Team ID: */! d
-        s///
-        q
-    ' "$1/$info_file"
+# $1: netid
+student_tag_ () {
+    {
+        print_student_property $1 first
+        print_student_property $1 last
+    } | sed -E 's/ /-/g; s/[^[:alnum:]]//g'
 }
 
 # $1: hw
@@ -31,16 +72,8 @@ grading_base () {
 max_team_id () {
     local dir
     for dir in $(grading_base $1)*; do
-        team_id "$dir" 2>/dev/null
+        repo_team_id "$dir" 2>/dev/null
     done | sort -nr | head -1
-}
-
-# $1: netid
-student_tag () {
-    {
-        print_student_property $1 first
-        print_student_property $1 last
-    } | sed -E 's/ /-/g; s/[^[:alnum:]]//g'
 }
 
 # $1: hw
