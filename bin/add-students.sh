e68cf7130ff85f602f06f2245a2bd3b74dc98c14
Jesse A. Tov <jesse.tov@gmail.com>
2020-04-19 16:07:02 -0500

    touch ups.

--- bin/add-students.sh
+++ bin/add-students.sh
@@ -7,11 +7,11 @@
 echo 'Input format: NetID Email Last Name,First Name' >&2
 
 while read netid email name; do
-    netid="$(echo $netid | tr A-Z a-z)"
-    last="$(echo "$name" | sed 's/,.*//')"
-    first="$(echo "$name" | sed 's/.*,//')"
-    "$COURSE_BIN"/set_student_property.sh -c "$netid" email "$email"
-    "$COURSE_BIN"/set_student_property.sh "$netid" last "$last"
-    "$COURSE_BIN"/set_student_property.sh "$netid" first "$first"
-    print_student_info "$netid"
+    netid=$(echo $netid | tr A-Z a-z)
+    last=$(echo $name | sed 's/,.*//')
+    first=$(echo $name | sed 's/.*,//')
+    "$COURSE_BIN"/set_student_property.sh -c $netid email "$email"
+    "$COURSE_BIN"/set_student_property.sh $netid last "$last"
+    "$COURSE_BIN"/set_student_property.sh $netid first "$first"
+    print_student_info $netid
 done
