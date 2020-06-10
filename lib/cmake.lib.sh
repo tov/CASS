7e355750eb2ccc85327bb14fda0180d8d796b846
Jesse A. Tov <jesse.tov@gmail.com>
2020-06-10 14:33:36 -0500

    Find add_executable in addition to add_program.

--- lib/cmake.lib.sh
+++ lib/cmake.lib.sh
@@ -1,19 +1,19 @@
 # CMake helpers
 
-perl_find_cmake_X_programs='
-    $p = "add$ARGV[0]_program";
+perl_find_cmake_command_arg1='
     $_ = join "", <STDIN>;
-    print "$1\n" while /\b$p\s*\(\s*([-_[:alnum:]]+)/ig
+    print "$1\n" while /\b$ARGV[0]\s*\(\s*([-_[:alnum:]]+)/ig
 '
 
-find_cmake_X_programs () {
-    perl -e "$perl_find_cmake_X_programs" "$1"
+find_cmake_command_arg1 () {
+    perl -e "$perl_find_cmake_command_arg1" "$1" < CMakeLists.txt
 }
 
 find_cmake_programs () {
-    find_cmake_X_programs '' < CMakeLists.txt
+    find_cmake_command_arg1 add_program
+    find_cmake_command_arg1 add_executable
 }
 
 find_cmake_test_programs () {
-    find_cmake_X_programs _test < CMakeLists.txt
+    find_cmake_command_arg1 add_test_program
 }
