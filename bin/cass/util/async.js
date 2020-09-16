bb9e74e66d0490013b986d7a18840229c07ee821
Jesse A. Tov <jesse.tov@gmail.com>
2020-09-16 09:41:09 -0500

    Fix seq_map to make it actually sequential.

--- bin/cass/util/async.js
+++ bin/cass/util/async.js
@@ -1,21 +1,18 @@
-const par = Promise.all.bind(Promise)
+const par_map = (f, xs) => Promise.all(xs.map(f))
 
-const seq = async xs => {
+const seq_map = async (f, xs) => {
   const result = []
 
   for (const x of xs)
-    result.push(await x)
+    result.push(await f(x))
 
   return result
 }
-
-const par_map = (f, xs) => par(xs.map(f))
-const seq_map = (f, xs) => seq(xs.map(f))
 
 const par_foreach = (xs, f) => par_map(f, xs)
 const seq_foreach = (xs, f) => seq_map(f, xs)
 
 module.exports = {
-  par, par_foreach, par_map,
-  seq, seq_foreach, seq_map,
+  par_foreach, par_map,
+  seq_foreach, seq_map,
 }
