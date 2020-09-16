18109f77c6811eee8fefef962e65ce33a0d01ae3
Jesse A. Tov <jesse.tov@gmail.com>
2020-09-15 21:10:00 -0500

    Assignments slugs (e.g. hw%02d) are now configurable, as is what we call them.
    e.g. "Homework 1" versus "Project 1"

--- lib/hw.lib.sh
+++ lib/hw.lib.sh
@@ -1,4 +1,7 @@
-# Shell library for dealing with homeworks
+# Shell library for dealing with assignments
+
+: ${assignment_slug_fmt:=hw%02d}
+: ${assignment_called:=Homework}
 
 find_local_repo () {
     eval "$(getargs + hw)"
@@ -9,23 +12,23 @@ find_local_repo () {
     if [ -d "$result" ]; then
         echo "$result"
     else
-        echo >&2 "Local repo for Homework $hw doesn’t exist"
+        echo >&2 "Local repo for ${assignment_called} $hw doesn’t exist"
         return 1
     fi
 }
 
 remote_starter_repo_name () {
-    printf '_hw%02d-starter' $1
+    printf "_${assignment_slug_fmt}-starter" $1
 }
 
 remote_starter_repo_url () {
-    printf '%s/_hw%02d-starter.git' "$git_base" $1
+    printf "%s/_${assignment_slug_fmt}-starter.git" "$git_base" $1
 }
 
 remote_hw_dev_repo_name () {
-    printf '_hw%02d-dev' $1
+    printf "_${assignment_slug_fmt}-dev" $1
 }
 
 remote_hw_dev_repo_url () {
-    printf '%s/_hw%02d-dev.git' "$git_base" $1
+    printf "%s/_${assignment_slug_fmt}-dev.git" "$git_base" $1
 }
