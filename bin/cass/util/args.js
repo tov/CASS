72a862361755328e64d0e750736622e4a1eab4ac
Jesse A. Tov <jesse.tov@gmail.com>
2020-09-17 14:14:48 -0500

    Using yargs for argument parsing. Common flags defined here.

--- bin/cass/util/args.js
+++ bin/cass/util/args.js
@@ -1,25 +1,85 @@
-const path = require('path')
+const path  = require('path')
 
-const pluralize = (count, singular, plural = `${singular}s`) =>
-  count == 1 ? singular : plural
+class Args {
+  _yargs = require('yargs')
 
-const counted = (count, singular, plural) =>
-  `${count} ${pluralize(count, singular, plural)}`
+  constructor(scriptName, params, descr) {
+    this._do(y => y
+      .strict()
+      .scriptName(scriptName)
+      .help()
+      .alias('h', 'help')
+      .version(false))
 
-exports.getArgs = ({min = 0, usage} = {}) => {
-  const {argv: [_, scriptPath, ...args]} = require('process')
-  const arg0 = path.basename(scriptPath)
-
-  if (/^-v|--verbose$/.test(args)) {
-    require('debug').enable('*')
-    args.shift()
+    if (params)
+      this._do(y => y.usage(`$0 ${params}`, descr, () => {}))
   }
 
-  if (args.length < min) {
-    throw usage
-      ? `Usage: ${arg0} ${usage}`
-      : `Error: ${arg0} requires at least ${counted(min, 'argument')}`
+  get argv() {
+    const argv = this._yargs.argv
+    if (argv.debug) require('debug').enable('*')
+    return argv
   }
 
-  return args
+  _option(name, opts) {
+    return this._do(y => y.option(name, opts))
+  }
+
+  _do(...fns) {
+    for (const fn of fns) {
+      this._yargs = fn(this._yargs)
+    }
+
+    return this
+  }
+
+  static _register(obj) {
+    for (const key in obj) {
+      this._register1(key, obj[key])
+    }
+  }
+
+  static _register1(method, {name, alias, ...opts}) {
+    this.prototype[method] = function() {
+      if (!name) name = method
+      if (!alias) alias = name[0]
+      return this._option(name, {alias, ...opts})
+    }
+  }
 }
+
+Args._register({
+  json: {
+    boolean: true,
+    description: 'Produce JSON output',
+  },
+
+  debug: {
+    alias: 'D',
+    boolean: true,
+    description: 'Print debugging output',
+  },
+
+  verbose: {
+    boolean: true,
+    description: 'Produce more output',
+  },
+
+  quiet: {
+    boolean: true,
+    description: 'Produce less output',
+  },
+
+  force: {
+    boolean: true,
+    description: 'Do it anyway',
+  },
+
+  dry_run: {
+    alias: 'N',
+    boolean: true,
+    description: "Don't actually change anything",
+  },
+})
+
+module.exports = Args
