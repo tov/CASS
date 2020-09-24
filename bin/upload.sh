79a1b1d7fda474390963992a123f11e7d9c25118
Jesse A. Tov <jesse.tov@gmail.com>
2020-09-24 16:43:50 -0500

    Upload from $COURSE_VAR/staging using rsync.

--- bin/upload.sh
+++ bin/upload.sh
@@ -2,19 +2,19 @@
 
 # upload.sh: uploads the course web site.
 #
-#  -m   upload man pages as well
-#  -W   skip uploading website; implies -m
+#  -m   upload man pages as well (-W && -S implies -m)
+#  -W   skip uploading website
+#  -S   skip uploading shell stuff
 #  -v   verbose
 
 . "$(dirname "$0")/.CASS"
-eval "$(getargs -mWv)"
+eval "$(getargs -mWSv)"
 
-src=web
-tempdir=/tmp/upload
+if [ -n "$flag_W" ] && [ -n "$flag_S" ]; then
+    flag_m=-m
+fi
 
-remove_tempdir () {
-    rm -Rf $tempdir
-}
+staging=$COURSE_VAR/staging
 
 assert_branch () {
     local actual
@@ -25,10 +25,10 @@ assert_branch () {
         exit 1
     }
 
-    if [[ "$actual" != "$desired" ]]; then
+    if [ "$actual" != "$desired" ]; then
         exec >&2
         echo "$(basename $0): cannot upload from this git branch"
-        if [[ "$dir" != . ]]; then
+        if [ "$dir" != . ]; then
             echo "  in submodule: $dir"
         fi
         echo "  branch is:    $actual"
@@ -37,34 +37,49 @@ assert_branch () {
     fi
 }
 
-
-upload_man_pages () {
-    : ${man_host? needs to be set in /etc/config.sh}
+upload_man () {
+    : ${shell_host? needs to be set in /etc/config.sh}
     : ${man_update_cmd? needs to be set in /etc/config.sh}
-    ssh "$man_host" $man_update_cmd
+
+    ssh "$shell_host" $man_update_cmd
 }
 
-upload_website () {
-    remove_tempdir
-    register_exit_function remove_tempdir
+upload_shell () {
+    : ${shell_host? needs to be set in /etc/config.sh}
+    : ${shell_path? needs to be set in /etc/config.sh}
 
-    "$COURSE_BIN"/publish.sh $flag_v $src $tempdir &&
-    rsync -avz --delete --chmod=a+rX $tempdir/ $web_host:$web_path
+    rsync -avz --chmod=a+rX --delete \
+        $staging/shell/ \
+        cs211@$shell_host:$shell_path
+}
+
+upload_web () {
+    : ${web_host? needs to be set in /etc/config.sh}
+    : ${web_path? needs to be set in /etc/config.sh}
+
+    rsync -avz --chmod=a+rX --delete \
+        $staging/web/ \
+        $web_host:$web_path
 }
 
 cd "$COURSE_ROOT"
 
-assert_branch .                         refs/heads/master
-assert_branch dev/lab                   refs/heads/master
-assert_branch lib/dot-cs211             refs/heads/master
-assert_branch lib/dot-cs211/lib/ge211   refs/heads/release
-assert_branch lib/dot-cs211/lib/catch   refs/heads/master
+# assert_branch .                         refs/heads/master
+# assert_branch dev/lab                   refs/heads/master
+# assert_branch lib/dot-cs211             refs/heads/master
+# assert_branch lib/dot-cs211/lib/ge211   refs/heads/release
+# assert_branch lib/dot-cs211/lib/catch   refs/heads/master
 
-if [[ -n "$flag_m$flag_W" ]]; then
-    upload_man_pages
+if [ -z "$flag_W" ]; then
+    upload_web &
 fi
 
-if [[ -z "$flag_W" ]]; then
-    upload_website
+if [ -z "$flag_S" ]; then
+    upload_shell &
 fi
 
+if [ -n "$flag_m" ]; then
+    upload_man &
+fi
+
+wait
